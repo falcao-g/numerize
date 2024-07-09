8c62c7495911aa7c73361a2302ef96d302799798
Falcão <gabriel.p.falcao50@gmail.com>
2024-07-09 02:12:49 -0400

    tada: first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+<h1 align="center"> NUMERIZE </h1>
+
+> **numerize is a very useful package that converts words to numbers**
+
+<p align="center">
+    <a href="https://www.npmjs.com/package/numerize"><img src="https://img.shields.io/npm/v/numerize.svg?maxAge=3600" alt="npm version" /></a>
+		<a href="https://www.npmjs.com/package/numerize"><img src="https://img.shields.io/npm/dt/numerize.svg?maxAge=3600" alt="npm downloads" /></a>
+    <a title="Support server" href="https://discord.gg/8WrAtVYVKR">
+        <img src="https://img.shields.io/discord/742332099788275732.svg?&logo=discord&logoColor=ffffff&color=7389D8&labelColor=6A7EC2&label=Support" alt="Support server">
+    </a>
+        <a title="Stars" href="https://github.com/falcao-g/falbot">
+        <img src="https://img.shields.io/github/stars/falcao-g/numerize" alt="Stars">
+    </a>
+</p>
+
+## **✨ Why numerize?**
+
+- Easy to use & complete.
+- Extremely useful to game-related projects.
+- Customizable.
+- Comes with 0 dependencies.
+
+## **⚙️ Installation**
+
+```bash
+npm i numerize
+```
+
+## **📚 Usage**
+
+Starting a game with Falgames is as easy as the following example:
+
+```js
+const { numerize } = require("numerize")
+
+console.log(numerize("10%", 100)) // 10
+console.log(numerize("1m", 100)) // 1000000
+console.log(numerize("half", 1000)) // 500
+console.log(numerize("all", 1000)) // 1000
+```
---
+++ index.js
@@ -0,0 +1,69 @@
+const words = {
+	all: 100,
+	half: 50,
+	tudo: 100,
+	metade: 50,
+	todo: 100,
+	mitad: 50,
+}
+
+const suffixes = {
+	k: 1000,
+	m: 1000000,
+	b: 1000000000,
+}
+
+/**
+ *
+ * @param {Object} newWords
+ * @description Adds new words to the dictionary
+ * @example addWords({ quarter: 25}) // { all: 100, half: 50, tudo: 100, metade: 50, todo: 100, mitad: 50, quarter: 25 }
+ */
+function addWords(newWords) {
+	Object.assign(words, newWords)
+}
+
+/**
+ *
+ * @param {Object} newWords
+ * @description Adds new suffixes to the dictionary
+ * @example addWords({ c: 100}) // { k: 1000, m: 1000000, b: 1000000000, c: 100 }
+ */
+function addSuffixes(newSuffixes) {
+	Object.assign(suffixes, newSuffixes)
+}
+
+/**
+ *
+ * @param {string} string
+ * @param {integer} total
+ * @description Parses a string to an integer
+ * @example specialArg('10%', 1000) // 100
+ * @returns {integer}
+ */
+function numerize(string, total) {
+	string = string.toLowerCase()
+	string.replace(/,/g, "")
+	string.replace(/\./g, "")
+	var new_value = parseInt(string)
+
+	if (string in words) {
+		new_value = (words[string] * total) / 100
+	} else if (string.slice(-1) in suffixes) {
+		new_value = parseInt(string.slice(0, -1)) * suffixes[string.slice(-1)]
+	} else if (string.slice(-1) == "%") {
+		new_value = parseInt((parseInt(string.slice(0, -1)) * parseInt(total)) / 100)
+	}
+
+	if (parseInt(new_value) <= 0 || isNaN(parseInt(new_value))) {
+		throw Error("Invalid value!")
+	} else {
+		return parseInt(new_value)
+	}
+}
+
+module.exports = {
+	addWords,
+	addSuffixes,
+	numerize,
+}
